b071be724387b2f596b3f8923e7680eaa9ba20e0
Maria Bogomaz <bogomazm@gmail.com>
2019-12-06 13:44:52 -0700

    initial commit

---
+++ src/components/MovieFilter.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <div id="movie-filter">
+    <h2>Filter Results </h2>
+    <div class="filter-group">
+      <check-filter v-for="genre in genres" :key="genre.key"></check-filter>
+    </div>
+  </div>
+</template>
+
+<script>
+import CheckFilter from './CheckFilter.vue'
+import genres from '@/util/genres.js'
+
+export default {
+  components: {
+    CheckFilter
+  },
+  data () {
+    return {
+      genres
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/components/MovieList.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div id="movie-list">
+    <div class="movie" v-for="movie in movies" :key="movie.title">
+      {{ movie.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      movies: [
+        { title: 'Pulp Fiction' },
+        { title: 'Home Alone' },
+        { title: 'Austin Powers' },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
